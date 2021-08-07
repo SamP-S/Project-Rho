6b492f512ffde83d4bcb25bbd37fc57fa826aaa1
Dinesh1503 <dineshkumar1552003@gmail.com>
2021-08-07 16:05:19 +0400

    Generates Maze and The Floor

--- Assets/scripts/Maze_Renderer.cs
+++ Assets/scripts/Maze_Renderer.cs
@@ -16,39 +16,69 @@ public class Maze_Renderer : MonoBehaviour
     private float size = 1f;
 
     [SerializeField]
+    private Transform Floorprefab = null;
+    
+    [SerializeField]
     private Transform wallPrefab = null;
+    [SerializeField]
+    private Transform entrancePrefab = null;
+    [SerializeField]
+    private Transform exitPrefab = null;
     public void Draw_Maze(Wall[,] maze)
     {
+        var floor = Instantiate(Floorprefab, transform);
+        floor.localScale = new Vector3(10,1,10);
+
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
             {
                 var cell = maze[i,j];
                 var position = new Vector3(-m/2+i, 0, -n/2+j);
-
+                
                 if(cell.HasFlag(Wall.UP))
                 {
                     var top_wall = Instantiate(wallPrefab, transform) as Transform;
                     top_wall.position = position + new Vector3(0, 0, size / 2);
                     top_wall.localScale = new Vector3(size, top_wall.localScale.y, top_wall.localScale.z);
-
                 }
                 if (cell.HasFlag(Wall.LEFT))
                 {
-                    var leftWall = Instantiate(wallPrefab, transform) as Transform;
-                    leftWall.position = position + new Vector3(-size / 2, 0, 0);
-                    leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
-                    leftWall.eulerAngles = new Vector3(0, 90, 0);
+                    if(i == 0 && j == 0)
+                    {
+                        var leftWall = Instantiate(entrancePrefab, transform) as Transform;
+                        leftWall.position = position + new Vector3(-size, 0, -size/2);
+                        leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
+                        leftWall.eulerAngles = new Vector3(0, 0, 0);
+                    }
+                    else
+                    {
+                        var leftWall = Instantiate(wallPrefab, transform) as Transform;
+                        leftWall.position = position + new Vector3(-size / 2, 0, 0);
+                        leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
+                        leftWall.eulerAngles = new Vector3(0, 90, 0);
+                    }
                 }
 
                 if (i == m - 1)
                 {
                     if (cell.HasFlag(Wall.RIGHT))
                     {
-                        var rightWall = Instantiate(wallPrefab, transform) as Transform;
-                        rightWall.position = position + new Vector3(+size / 2, 0, 0);
-                        rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
-                        rightWall.eulerAngles = new Vector3(0, 90, 0);
+                        if(j == n - 1)
+                        {
+                            var rightWall = Instantiate(exitPrefab, transform) as Transform;
+                            rightWall.position = position + new Vector3(+size / 2, 0, size/2);
+                            rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
+                            rightWall.eulerAngles = new Vector3(0, 0, 0);
+                        }
+                        else
+                        {
+                            var rightWall = Instantiate(wallPrefab, transform) as Transform;
+                            rightWall.position = position + new Vector3(+size / 2, 0, 0);
+                            rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
+                            rightWall.eulerAngles = new Vector3(0, 90, 0);
+                        }
+                        
                     }
                 }
 
@@ -69,6 +99,8 @@ public class Maze_Renderer : MonoBehaviour
     {
         var maze = Maze_Generator.Create_Maze(m,n);
         Draw_Maze(maze);
+        // path_finder.initial_call(maze,m,n);
+        
     }
 
     // Update is called once per frame
