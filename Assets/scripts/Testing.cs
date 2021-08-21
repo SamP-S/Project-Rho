ba371b9758ee61c9aa22b4c17bb636daa0b1a048
Dinesh1503 <dineshkumar1552003@gmail.com>
2021-08-21 11:21:27 +0400

    All Files

--- Assets/scripts/Testing.cs
+++ Assets/scripts/Testing.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 using System.Collections;
 public class Testing : MonoBehaviour
 {
-   public static void Main()
-   {
-      string path = @"D:\Maze Game Project\Project-Rho\Assets\Mazes\Test.txt";
-      string content = "Just Created this Test.txt File";
-      if(!File.Exists(path))
-      {
-         File.WriteAllText(path,"Login Log \n\n");
-      }
+   void Start() {
       
    }
 }
